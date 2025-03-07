b3431d8b6358e6e99cfa4cffc41bdd544bd7491f
vianey <vianeylinares@gmail.com>
2025-03-06 17:02:37 -0800

    At form-popup.php and style.css: - Changed HTML structure for form display to specify styles. - Added styles in a more specific manner for the form and input fields.

--- form-popup.php
+++ form-popup.php
@@ -19,7 +19,6 @@ function magnific_popup_enqueue_files(){
     wp_register_style( 'magnific-popup', plugin_dir_url( __FILE__ ) . 'magnific-popup/magnific-popup.css' );
     wp_register_style( 'style', plugin_dir_url( __FILE__ ) . 'style.css' );
 
-    //wp_register_script( 'jquery' );
     wp_register_script( 'magnific-popup', plugin_dir_url( __FILE__ ) . 'magnific-popup/jquery.magnific-popup.js', array(), '', true );
     wp_register_script( 'js', plugin_dir_url( __FILE__ ) . 'js.js', array(), '', true );
 
@@ -39,50 +38,39 @@ function form_and_popup_shortcode(){
     wp_enqueue_script( 'js' );
     
     ?>
-		
-    	<form>
-    		
-    		<input type="text" name="name" placeholder="Name *" required=""><br/>
-    		<input type="email" name="email" placeholder="E-mail *" required=""><br/>
-    		<input type="checkbox" name="policy" id="policy-check" required=""> I have read and accept the <a href="<?php echo home_url(); ?>/privacy-policy/" target="_blank">privacy policy</a> <br/>
-    		<input type="submit" name="send" value="Send">
-
-    	</form>    
+		<div class="form-box">
+            <form>
+                <input type="text" name="name" placeholder="Name *" required=""><br/>
+                <input type="email" name="email" placeholder="E-mail *" required=""><br/>
+                <input type="checkbox" name="policy" id="policy-check" required=""> I have read and accept the <a href="<?php echo home_url(); ?>/privacy-policy/" target="_blank">privacy policy</a> <br/>
+                <input type="submit" name="send" value="Send">
+            </form>
+        </div>
             
         <a class="popup-with-zoom-anim" id="politic-popup" href="#small-dialog" style="display: none;">Open with fade-zoom animation</a>
 
         <div id="small-dialog" class="zoom-anim-dialog mfp-hide" style="text-align: left; color: #000;">
-        
         	<?php
-
                 $args = array(
                     'post_type' => 'any',
                     'p' => 3, // Privacy policy page ID number
                 );
-
                 $query = new WP_Query($args);
 
                 while($query->have_posts()) : $query->the_post();
-                
-                    ?>                  
-
+                    ?>
                         <div>
                             <?php the_title(); ?>
                         </div>
                         <div>
                             <?php the_content(); ?>
                         </div>
-                                
-                    <?php           
-        
+                    <?php
                 endwhile; wp_reset_postdata();
-
             ?>
-
         </div>
 
 		<?php
-    
 	return ob_get_clean();
 
 }
